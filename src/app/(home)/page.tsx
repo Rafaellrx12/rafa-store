67f5021d406eee79a1c550f7709f1b4ce7195ea7
Rafaellrx12 <rafaeloficial555@gmail.com>
2023-10-18 20:25:50 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,7 @@
-
 import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/product-list";
-
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
     where: {
@@ -12,24 +10,33 @@ export default async function Home() {
       },
     },
   });
-
-
-
   return (
     <div>
       <Image
         src="/banner-home-01.png"
         height={0}
         width={0}
-        className="h-auto w-full"
+        className="h-auto w-full px-5"
         sizes="100vw"
         alt="Até 55% de desconto esse mês!"
       />
+      <div className="mt-8 px-5">
+        <Categories />
+      </div>
 
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
-    </div>
-  )
 
-  }
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses!"
+      />
+    </div>
+  );
+}
