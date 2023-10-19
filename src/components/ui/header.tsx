15598c407004c0704a897bd8e82fe42ecf242c68
Rafaellrx12 <rafaeloficial555@gmail.com>
2023-10-19 16:14:16 -0300

    Feat: ensure user goes to catalog page on catalog button click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import {
   HomeIcon,
   ListOrderedIcon,
@@ -11,23 +10,27 @@ import {
 } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarImage } from "./avatar";
 import { AvatarFallback } from "@radix-ui/react-avatar";
 import { Separator } from "./separator";
+import Link from "next/link";
 
 const Header = () => {
   const { status, data } = useSession();
-
   const handleLoginClick = async () => {
     await signIn();
   };
-
   const handleLogoutClick = async () => {
     await signOut();
   };
-
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
       <Sheet>
@@ -36,12 +39,10 @@ const Header = () => {
             <MenuIcon />
           </Button>
         </SheetTrigger>
-
         <SheetContent side="left">
           <SheetHeader className="text-left text-lg font-semibold">
             Menu
           </SheetHeader>
-
           {status === "authenticated" && data?.user && (
             <div className="flex flex-col">
               <div className="flex items-center gap-2 py-4">
@@ -49,20 +50,16 @@ const Header = () => {
                   <AvatarFallback>
                     {data.user.name?.[0].toUpperCase()}
                   </AvatarFallback>
-
                   {data.user.image && <AvatarImage src={data.user.image} />}
                 </Avatar>
-
                 <div className="flex flex-col">
                   <p className="font-medium">{data.user.name}</p>
-                  <p className="text-sm opacity-75">Nelson Vem Logar Teu Email Aqui </p>
+                  <p className="text-sm opacity-75">Boas compras!</p>
                 </div>
               </div>
-
               <Separator />
             </div>
           )}
-
           <div className="mt-4 flex flex-col gap-2">
             {status === "unauthenticated" && (
               <Button
@@ -74,7 +71,6 @@ const Header = () => {
                 Fazer Login
               </Button>
             )}
-
             {status === "authenticated" && (
               <Button
                 onClick={handleLogoutClick}
@@ -85,34 +81,36 @@ const Header = () => {
                 Fazer Logout
               </Button>
             )}
-
             <Button variant="outline" className="w-full justify-start gap-2">
               <HomeIcon size={16} />
               Início
             </Button>
-
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size={16} />
               Ofertas
             </Button>
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href="/catalog">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <ListOrderedIcon size={16} />
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
-
       <h1 className="text-lg font-semibold">
-        <span className="text-primary font-extrabold text-xl">Emissaries</span> Store
+        <span className="text-primary">FSW</span> Store
       </h1>
-
       <Button size="icon" variant="outline">
         <ShoppingCartIcon />
       </Button>
     </Card>
   );
 };
-
 export default Header;
